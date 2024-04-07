97e3f21c1687b11cd9f42ce79caf36be0d9332c4
torekhanmukhtarov <nahkerot@gmail.com>
2024-04-07 13:17:16 +0500

    Initial Commit

---
+++ EShop AUTH/EShop_AUTHApp.swift
@@ -0,0 +1,17 @@
+//
+//  EShop_AUTHApp.swift
+//  EShop AUTH
+//
+//  Created by Torekhan Mukhtarov on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EShop_AUTHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EShop AUTHUITests/EShop_AUTHUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EShop_AUTHUITestsLaunchTests.swift
+//  EShop AUTHUITests
+//
+//  Created by Torekhan Mukhtarov on 07.04.2024.
+//
+
+import XCTest
+
+final class EShop_AUTHUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
